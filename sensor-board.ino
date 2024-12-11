f2012ed0b81e135c609caa7d6f9ce062c581ffcf
@Alidat <alvinnahereza@gmail.com>
2024-12-11 08:40:13 +0300

    sensor board

--- sensor-board.ino
+++ sensor-board.ino
@@ -24,4 +24,26 @@ void sendDistance() {
   Wire.write(distance & 0xFF);
 }
 
+void loop() {
+  delay(100);
+}
+
+void sendDistance() {
+  long duration = getUltrasonicDistance();
+  int distance = duration * 0.034 / 2;
+  Wire.write(distance >> 8);
+  Wire.write(distance & 0xFF);
+}
+
+long getUltrasonicDistance() {
+  // Trigger the ultrasonic sensor using direct register access
+  PORTD &= ~(1 << TRIG_PIN); // Set TRIG_PIN LOW
+  delayMicroseconds(2);
+  PORTD |= (1 << TRIG_PIN); // Set TRIG_PIN HIGH
+  delayMicroseconds(10);
+  PORTD &= ~(1 << TRIG_PIN); // Set TRIG_PIN LOW
+
+  // Measure the pulse duration on ECHO_PIN
+  return pulseIn(ECHO_PIN, HIGH);
+}
 
