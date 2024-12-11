daec0ab4ad690c0dba4584d4798ac3d1c4e190fa
@Alidat <alvinnahereza@gmail.com>
2024-12-11 08:36:21 +0300

    sensor board

--- sensor-board.ino
+++ sensor-board.ino
@@ -4,3 +4,12 @@
 #define TRIG_PIN 3
 #define ECHO_PIN 2
 
+void setup() {
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onRequest(sendDistance);
+
+  // Set TRIG_PIN as OUTPUT and ECHO_PIN as INPUT using direct register access
+  DDRD |= (1 << TRIG_PIN); // Set TRIG_PIN as OUTPUT
+  DDRD &= ~(1 << ECHO_PIN); // Set ECHO_PIN as INPUT
+}
+
