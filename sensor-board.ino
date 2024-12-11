8dde25979ab1549adc876ebd9b239d7b4b99a5cf
@Alidat <alvinnahereza@gmail.com>
2024-12-11 08:38:55 +0300

    sensor board

--- sensor-board.ino
+++ sensor-board.ino
@@ -13,3 +13,15 @@ void setup() {
   DDRD &= ~(1 << ECHO_PIN); // Set ECHO_PIN as INPUT
 }
 
+void loop() {
+  delay(100);
+}
+
+void sendDistance() {
+  long duration = getUltrasonicDistance();
+  int distance = duration * 0.034 / 2;
+  Wire.write(distance >> 8);
+  Wire.write(distance & 0xFF);
+}
+
+
